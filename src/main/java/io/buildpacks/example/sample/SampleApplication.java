d4edc51b3112c4f1b0e6c85113b20566f707f38b
Akash18 <73993188+ak20102763@users.noreply.github.com>
2022-03-23 10:51:40 +0530

    Update SampleApplication.java

--- src/main/java/io/buildpacks/example/sample/SampleApplication.java
+++ src/main/java/io/buildpacks/example/sample/SampleApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SampleApplication {
-        System.out.println("");
+        
 	public static void main(String[] args) {
+		System.out.println("");
 		SpringApplication.run(SampleApplication.class, args);
 	}
 
